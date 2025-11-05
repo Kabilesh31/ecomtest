be124ef6787b11779178f3b80e11cc1a5486b990
Kabilesh31 <kabigoarge@gmail.com>
2025-11-06 01:50:22 +0530

    all checked

---
+++ types/order.ts
@@ -0,0 +1,28 @@
+// /types/order.ts
+export interface PurchasedProduct {
+  productId: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export interface CustomerDetails {
+  firstName: string;
+  lastName?: string;
+  email?: string;
+  phone?: string;
+  address?: string;
+}
+
+export interface Order {
+  _id: string;
+  razorpayPaymentId: string;
+  razorpayOrderId?: string;
+  razorpaySignature?: string;
+  totalAmount: number;
+  customerId?: string;
+  createdAt: string;
+  purchasedProducts: PurchasedProduct[];
+  customerDetails: CustomerDetails;
+  status: string; 
+}
